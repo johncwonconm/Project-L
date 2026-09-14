728a9a3786ccf2604cf4019e4baaa952e6e93803
johncwonconm <xifyfncs@gmail.com>
2026-09-13 20:28:58 -0400

    Update get-script.js

--- api/get-script.js
+++ api/get-script.js
@@ -1,1 +1,16 @@
+export default function handler(req, res) {
+  const SECRET_KEY = "P249587n2gn238097bo4kn";
+  const userKey = req.query.key;
 
+  if (userKey !== P249587n2gn238097bo4kn) {
+    return res.status(403).send("Access Denied: Invalid Key");
+  }
+
+  // PASTE YOUR ENTIRE PROJECT-L-MAIN CODE BELOW
+  const luaScript = `
+    -- Your real Lua script code goes here
+  `;
+
+  res.setHeader('Content-Type', 'text/plain');
+  return res.status(200).send(luaScript);
+}
