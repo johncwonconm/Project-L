8a4d3a822596142ed5d7a8a9981ff89bafc56776
johncwonconm <xifyfncs@gmail.com>
2026-09-13 20:16:19 -0400

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -4,5 +4,4 @@ if (getgenv().PROJECT_L_LOADED) then
 end
 getgenv().PROJECT_L_LOADED = true;
 
--- Fetch and execute your hidden code from your Vercel API
-loadstring(game:HttpGet("https://project-l-one-lac.vercel.app/api/get-script?key=P249587n2gn238097bo4kn"))();
+ERROR INVALID ID PLEASE TRY AGAIN
